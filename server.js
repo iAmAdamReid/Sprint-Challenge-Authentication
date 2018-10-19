8d48d3e18bb6a012880823014b7d5ecb68ab73a2
iAmAdamReid <adam@adamreid.me>
2018-10-19 11:45:03 -0700

    update cors object for safety

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const corsOptions = {
   // If you're moving onto the stretch problem you'll need to set this obj with the appropriate fields
   // ensure that your client's URL/Port can achieve a Handshake
   // then pass this object to the cors() function
+  origin: 'http://localhost:3000',
 };
 
 server.use(express.json());
-server.use(cors());
+server.use(cors(corsOptions));
 server.use(helmet());
 
 function logger(req, res, next){
